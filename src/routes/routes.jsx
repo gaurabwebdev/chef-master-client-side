c8c8cf6cdcfe92837e4a63d4965e997f529be8c9
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-02 01:39:21 +0600

    some more routes added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Main from "../Layouts/Main";
 import Home from "../components/Home";
+import Blog from "../components/Blog";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +15,18 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
