28cfbe415e1855c6678e402fbf25a90324ca4920
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-02 01:03:28 +0600

    firebase installed and authentication providers activated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <p></p>
+      <p>Hello</p>
     </div>
   );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChOH0pT0FEIMCV8bxzHY2XlCrUNcmwvJ8",
+  authDomain: "chef-recipe-hunter-68eef.firebaseapp.com",
+  projectId: "chef-recipe-hunter-68eef",
+  storageBucket: "chef-recipe-hunter-68eef.appspot.com",
+  messagingSenderId: "318371623848",
+  appId: "1:318371623848:web:f85415d00b97f21301239d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
